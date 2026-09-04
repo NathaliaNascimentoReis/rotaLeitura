c2530d3ede8e26145b53a03d4c10df3a85dd1931
Nath <nathalia.n.reis@aluno.senai.br>
2026-09-04 10:48:00 -0300

    feat: cria página de edição de livros

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import BookScreen from './src/screens/BookScreen.js';
 import CreateScreen from './src/screens/CreateScreen.js';
 import HomeScreen from './src/screens/HomeScreen.js';
 import SignInScreen from './src/screens/SignInScreen.js';
+import EditScreen from './src/screens/EditScreen.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function App() {
             <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Book" component={BookScreen} />
                 <Stack.Screen name="Create" component={CreateScreen} />
+                <Stack.Screen name="Edit" component={EditScreen} />
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="SignIn" component={SignInScreen} />
             </Stack.Navigator>
